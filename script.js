535deb68185c9fd4f91032121b3f79967a467090
Zachary Crespin <Zachary.lights@gmail.com>
2023-02-21 14:08:58 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+fetch("https://random-quote-ta5ihvbchq-uc.a.run.app")
+  .then((response) => response.json())
+  .then((data) => {
+    document.getElementById("quoteQuote").innerText = `“${data.quote}”`;
+    if (data.author != "") {
+      document.getElementById("quoteAuthor").innerText = `― ${data.author}`;
+      document.title = `“${data.quote}” ― ${data.author}`;
+    } else {
+      document.title = `“${data.quote}”`;
+    }
+  });
